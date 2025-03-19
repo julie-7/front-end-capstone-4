0add7e97a3e652b7a79e3496e1d1b18a7335acb8
“julie-7” <juliemouton23@gmail.com>
2025-03-18 20:44:50 -0500

    my next commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { RegisterForm } from "./component/registerform.jsx";
 import { LoginForm } from "./component/loginform.jsx";
 import { Notfound } from "./component/notfound.jsx";
+import{menu} from "./Screencomponents/"
 
 export const App = () => {
   return (
@@ -13,8 +14,30 @@ export const App = () => {
         <Route path="*" element={<Notfound />} />
       </Routes>
     </Router>
-  );
-};
+  )
+
+    useEffect(() => {
+
+      token = window.localStorage.getItem(`todolist`)
+      validateToken()
+    }, [])
+
+  return (
+    <>
+      <BrowserRouter>
+        {isLogin ? <menu /> : ""}
+        <Routes path='/' element={<LogIn />}>
+          {isLogin ? <Route path='/dashboard' element={<DashBoard/>} /> : ""}
+          {isLogin ? <Route path='/earning' element={<Earnings/>} /> : ""}
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
+
+
 
 
 
---
+++ src/ScreenComponent/Earning.jsx
@@ -0,0 +1,16 @@
+import { EarningForm } from "/components/EarningForm"
+import { EarningTable } from "/components/EarningTable"
+
+export const Earning = () => {
+    return (
+<>
+            <main className="container">
+                <main className="container">
+                    <EarningForm />
+                    <EarningTable />
+                </main>
+            
+            
+            </>
+            )
+    }
