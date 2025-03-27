d10320b417a93cf3ac8104e59286c1b0585504a0
frlopez88 <frlopez88@icloud.com>
2025-03-26 18:52:25 -0600

    correction task form

--- src/component/TaskForm.jsx
+++ src/component/TaskForm.jsx
@@ -2,44 +2,48 @@ import React, { useState } from 'react'
 import { Await } from 'react-router-dom'
 
 export const TaskForm = () => {
-  return (
-    <div>TaskForm</div>
-  )
+
   const [formData, setFormData] = useState("")({
     taskId: "",
     task: "",
-    description:""
+    description: ""
   })
-const[notification, setNotification]= useState("")
-const[task, settask] = useState([])
+  const [notification, setNotification] = useState("")
+  const [task, settask] = useState([])
 
-const baseUrl = import.meta.env.VITE_BASE_URL  
+  const baseUrl = import.meta.env.VITE_BASE_URL
 
-const onChangeHandler = (event) => {
-  const property = event.target.name
-  const value = event.target.value 
-  const tmpObject = formData
- tmpObject[property] = value
-  setFormData(tmpObject)
-}
-const submitHandler = async (event) => {
-  const token = window.localStorage.getItem("task-credential")
-
-  event.preventDefault()
-
-  const endpoint = 'task'
-  const newUrl = `${baseUrl}${endpoint}`
-
-  console.log(newUrl)
-  console.log(formData)
-}
-const response = async fetch (newUrl, {
-  method: "POST",
-  headers:{
-    'Content-Type': 'application/json',
-    
+  const onChangeHandler = (event) => {
+    const property = event.target.name
+    const value = event.target.value
+    const tmpObject = formData
+    tmpObject[property] = value
+    setFormData(tmpObject)
   }
-}
+  const submitHandler = async (event) => {
+    const token = window.localStorage.getItem("task-credential")
 
-)
+    event.preventDefault()
+
+    const endpoint = 'task'
+    const newUrl = `${baseUrl}${endpoint}`
+
+    console.log(newUrl)
+    console.log(formData)
+
+    const response = await fetch(newUrl, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+
+      }
+    })
+
+
+  }
+
+
+  return (
+    <div>TaskForm</div>
+  )
 }
