be23f664bfc0d8203fa0e214efa2efd178302eec
julie-7 <juliemouton23@gmail.com>
2025-03-27 18:38:04 -0500

    my next commit

--- src/component/TaskForm.jsx
+++ src/component/TaskForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Await } from 'react-router-dom'
 
 export const TaskForm = () => {
@@ -35,15 +35,47 @@ export const TaskForm = () => {
       method: "POST",
       headers: {
         'Content-Type': 'application/json',
-
-      }
+        'Authorization': token
+      },
+      body: JSON.stringify(formData)
     })
+    if(response.ok){
+      setNotification("Task Created")
 
+      setTimeout(() =>{
+        window.location.href = "/task"
+      }, 2000)
+    }else {
+      const errMessage = await response.json()
+      console.log(errMessage)
+      setNotification(errMessage.error)
+    }
 
   }
 
+  const gettask = async () => {
+    const token = window.localStorage.getItem("task-credential")
+    const endpoint = 'task'
+    const newUrl = `${baseUrl}${endpoint}`
+  },
 
+  const response = await fetch(newUrl,{
+    method:"GET",
+    headers: {
+      'content-Type': 'application/json',
+      'Authorization': token
+    }
+    
+    })
+
+    if(response.ok) {
+      const data = await response.json()
+      settask(data)
+    }else {
+     setNotification("Error on fetching task")
+    }
+}
   return (
     <div>TaskForm</div>
   )
-}
+
