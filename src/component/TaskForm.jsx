d5163321f764d48df33a90de4c5bc255a0437111
julie-7 <juliemouton23@gmail.com>
2025-03-26 19:48:52 -0500

    my next commit

--- src/component/TaskForm.jsx
+++ src/component/TaskForm.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Await } from 'react-router-dom'
 
 export const TaskForm = () => {
   return (
     <div>TaskForm</div>
   )
+  const [formData, setFormData] = useState("")({
+    taskId: "",
+    task: "",
+    description:""
+  })
+const[notification, setNotification]= useState("")
+const[task, settask] = useState([])
+
+const baseUrl = import.meta.env.VITE_BASE_URL  
+
+const onChangeHandler = (event) => {
+  const property = event.target.name
+  const value = event.target.value 
+  const tmpObject = formData
+ tmpObject[property] = value
+  setFormData(tmpObject)
+}
+const submitHandler = async (event) => {
+  const token = window.localStorage.getItem("task-credential")
+
+  event.preventDefault()
+
+  const endpoint = 'task'
+  const newUrl = `${baseUrl}${endpoint}`
+
+  console.log(newUrl)
+  console.log(formData)
+}
+const response = async fetch (newUrl, {
+  method: "POST",
+  headers:{
+    'Content-Type': 'application/json',
+    
+  }
+}
+
+)
 }
